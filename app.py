87c6306a62799c97bfdf78314a80e39135dac761
Praise Nnamonu <praise.nnamonu@kibo.school>
2023-03-08 11:06:50 +0100

    completed practice

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask, request
+from flask import Flask, jsonify
+# jsonify is used to convert data types into JSON format.
 
 app = Flask(__name__)
 
-@app.get("/users/<id>")
-def show_string():
-    return {}
-pass
+@app.route('/')
+def home():
+    return 'I am Preno'
 
-# This runs the app
-if __name__ == "__main__":
-    app.run()
+@app.route('/number')
+def number():
+    return jsonify(42)
+
+@app.route('/list')
+def some_list():
+    return jsonify(['Naruto', 'AOT', 'HxH'])
+
+@app.route('/dict')
+def some_dict():
+    return jsonify({'name': 'Eren', 'age': 27, 'city': 'Paradis'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
